3068c6edcdc83516c67948eaea2fe117a7fc8f9b
Server <tilesmaster@tiless-mbp.attlocal.net>
2018-01-05 01:02:16 -0500

    first

---
+++ hotRestaurant/app/data/waitinglist-data.js
@@ -0,0 +1,10 @@
+var waitingArray = [
+    {
+    	customerName: 'saima',
+    	customerEmail: "saima@example.com", 
+    	customerID:'saima234'
+    	phoneNumber: 000-000-0000
+    }
+];
+
+module.exports = waitingArray;
